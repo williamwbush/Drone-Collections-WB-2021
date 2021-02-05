f1d6e4cb4ff538a1fb07e0e6a8d041d93a03bb98
William Bush <wbush89@gmail.com>
2021-02-05 11:34:10 -0600

    deploy for Heroku

---
+++ app.py
@@ -0,0 +1,4 @@
+from drone_api import app, routes
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ drone_api/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+# TODO: Import Config Object for Flask Project
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+# Import for Flask Login
+from flask_login import LoginManager
+
+# import for authlib integrations
+from authlib.integrations.flask_client import OAuth
+
+# import for flask-marshmallow
+from flask_marshmallow import Marshmallow
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'signin' #Specify what page to load for NON-AUTHED users
+
+oauth = OAuth(app)
+
+ma = Marshmallow(app)
+
+from drone_api import routes, models
